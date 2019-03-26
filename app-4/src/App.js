48db9d45a215009bb924ff4e653390ecf52b8f55
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-26 16:01:27 -0500

    Done I Hope

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -3,19 +3,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        u: '', 
+        p: ''
+      };
+    // this.evenOddCalc = this.evenOddCalc.bind(this);
+}
+
+
+
+userN(val) {
+  this.setState({u: val});
+}
+pass(val) {
+  this.setState({p: val})
+}
+
+alertInfo() {
+  alert("User Name: " + this.state.u + "     Password: " + this.state.p);
+}
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+<input className="userName" onChange={ (e) => this.userN(e.target.value) } placeholder="User Name"></input>
+<input className="password" onChange={ (e) => this.pass(e.target.value) } placeholder="Password"></input>
+<button className="login" onClick={ () => { this.alertInfo(this.state.userInput) }}>Login</button>
+
+       </div>
     );
   }
 }
+
+
+
 
 export default App;
